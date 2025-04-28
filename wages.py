bd25334ab9a1d66a83f477e6b37dda122e3d5e93
Alan Harrison, Lecturer <133867300+AlanHarrisonADA@users.noreply.github.com>
2025-04-28 17:15:01 +0000

    Update wages.py to fix logic and add colours

--- wages.py
+++ wages.py
@@ -3,20 +3,23 @@ from colored import Fore, Back, Style
 hours = float(input("hours worked? "))
 target = input("target met (y/n)? ")
 
-double_time, overtime_hours = 0,0
+
 if hours > 60:
     double_time = hours - 60
-elif hours > 40:
     overtime_hours = hours - 40 - double_time 
+elif hours > 40:
+    double_time = 0
+    overtime_hours = hours - 40 
 else:
+    double_time = 0
     overtime_hours = 0
 
-base_hours = hours - overtime_hours
-wages = base_hours * 12 + overtime_hours * 12 * 1.5
+base_hours = hours - overtime_hours - double_time
+wages = base_hours * 12 + overtime_hours * 12 * 1.5 + double_time * 12 * 2
 if target == "y":
     wages += 50
 
 wages = max(wages, 20) 
-wages = min(wages, 100) 
+wages = min(wages, 600) 
 
 print(f'{Fore.white}{Back.green}wages = {wages}{Style.reset}')
